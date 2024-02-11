cba0ded85de2c0699d1a57fc0cc39a200ca278c3
bromivipo <misha.pototsky@gmail.com>
2024-02-11 21:00:25 +0300

    generic repo

--- product_engine/src/app/crud.py
+++ product_engine/src/app/crud.py
@@ -1,9 +1,11 @@
+from typing import Any
 from sqlalchemy.orm import Session
 from database import SessionLocal
 import json
 import datetime
 import models
 import random
+from functools import lru_cache
 
 class GenericRepository:
 
@@ -13,39 +15,24 @@ class GenericRepository:
 
     def get_all(self):
         return self.db.query(self.entity).all()
-           
-    def get_by_product_id(self, id):
-        return self.db.query(self.entity).filter(self.entity.product_id==id).first()
+    
+    def get_by_condition(self, condition):
+        return self.db.query(self.entity).filter(condition).all()
 
-    def check_product(self, id, name, version):
-        return self.db.query(self.entity).filter(self.entity.product_id == id or \
-                                      (self.entity.product_name == name and \
-                                       self.entity.product_version == version)).first()
-
-    def check_client(self, data):
-        return self.db.query(self.entity).filter(self.entity.birthday == data["birthday"] and self.entity.email == data["email"] and \
-                                    self.entity.full_name == data["full_name"] and self.entity.passport_details == data["passport_details"] and \
-                                    self.entity.phone_number == data["phone_number"] and self.entity.income == data["income"]).first()
+    def check_by_condition(self, condition):
+        return self.db.query(self.entity).filter(condition).first()
 
     def add(self, entity):
         self.db.add(entity)
         self.db.commit()
         self.db.refresh(entity)
 
-    def delete_product(self, id):
-        self.db.query(self.entity).filter(self.entity.product_id==id).delete()
+    def delete_by_condition(self, condition):
+        self.db.query(self.entity).filter(condition).delete()
         self.db.commit()
 
 
-def get_products(db: Session):
-    repo = GenericRepository(db, models.Product)
-    return repo.get_all()
-
-def get_product_by_code(db: Session, product_id: str):
-    repo = GenericRepository(db, models.Product)
-    return repo.get_by_product_id(product_id)
-
-def add_product(db: Session, data):
+def add_product(data, repo: GenericRepository):
     ints = ['min_term', 'max_term', 'min_principle_amount', 'max_principle_amount', 'min_origination_amount', 'max_origination_amount']
     doubles = ['min_interest', 'max_interest']
     new_product = json.loads(data)
@@ -60,20 +47,15 @@ def add_product(db: Session, data):
         except ValueError:
             return 400
     new_product = models.Product(new_product)
-    repo = GenericRepository(db, models.Product)
-    
-    if repo.check_product(new_product.product_id, new_product.product_name, new_product.product_version) is not None:
+    if repo.check_by_condition(models.Product.product_id == new_product.product_id \
+                                or (models.Product.product_name == new_product.product_name and models.Product.product_version == new_product.product_version)) is not None:
         return 409
     else:
         repo.add(new_product)
         return 200
 
 
-def delete_product_by_code(db: Session, product_id: str):
-    repo = GenericRepository(db, models.Product)
-    repo.delete_product(product_id)
-
-def create_agreement(db: Session, data):
+def create_agreement(repo: GenericRepository, repo2: GenericRepository, repo3: GenericRepository, data):
     info = json.loads(data)
     client = {}
     agreement = {}
@@ -81,8 +63,7 @@ def create_agreement(db: Session, data):
     client["passport_details"] = info["passport_number"]
     client["email"] = info["email"]
     client["phone_number"] = info["phone"]
-    repo = GenericRepository(db, models.Product)
-    prod = repo.get_by_product_id(info["product_code"])
+    prod = repo.get_by_condition(models.Product.product_id == info["product_code"])
 
     if prod is None:
         return 0
@@ -106,19 +87,20 @@ def create_agreement(db: Session, data):
     if agreement["principle_amount"] > prod.max_principle_amount or agreement["principle_amount"] < prod.min_principle_amount:
         return -4
     
-    client = check_client(db, client)
+    client = check_client(repo3, client)
     agreement["client_id"] = int(client)
     agreement["product_id"] = str(info["product_code"]) 
 
     new_agr = models.Agreement(agreement)
-    repo2 = GenericRepository(db, models.Agreement)
+    # repo2 = GenericRepository(db, models.Agreement)
     repo2.add(new_agr)
     return new_agr.agreement_id
 
 
-def check_client(db: Session, data):
-    repo = GenericRepository(db, models.Client)
-    client = repo.check_client(data)
+def check_client(repo: GenericRepository, data):
+    client = repo.check_by_condition(models.Client.birthday == data["birthday"] and models.Client.email == data["email"] and \
+                                    models.Client.full_name == data["full_name"] and models.Client.passport_details == data["passport_details"] and \
+                                    models.Client.phone_number == data["phone_number"] and models.Client.income == data["income"])
     if client is None:
         new = models.Client(data)
         repo.add(new)
@@ -126,3 +108,20 @@ def check_client(db: Session, data):
         return new.client_id
     else:
         return client.client_id
+
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+class create_repo:
+    def __init__(self, entity) -> None:
+        self.db: Session = SessionLocal()
+        self.entity = entity
+
+    @lru_cache
+    def __call__(self):
+        return GenericRepository(self.db, self.entity)
--- product_engine/src/app/main.py
+++ product_engine/src/app/main.py
@@ -1,37 +1,29 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-import crud, models
+from crud import create_repo, GenericRepository, add_product, create_agreement
+import models
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @app.get("/product", summary="Get a list of products")
-def read_products(db: Session = Depends(get_db)):
-    users = crud.get_products(db)
-    return users
+def read_products(repo: GenericRepository = Depends(create_repo(models.Product))):
+    prod = repo.get_all()
+    return prod
 
 
 @app.get("/product/{product_id}", summary="Get a certain product by id")
-def read_product(product_id: str, db: Session = Depends(get_db)):
-    db_product = crud.get_product_by_code(db, product_id=product_id)
-    if db_product is None:
+def read_product(product_id: str, repo: GenericRepository = Depends(create_repo(models.Product))):
+    db_product = repo.get_by_condition(models.Product.product_id==product_id)
+    if not db_product:
         raise HTTPException(status_code=404, detail="Product not found")
     return db_product
 
 @app.post("/product", summary="Add a new product")
-def post_product(data, db: Session = Depends(get_db)):
-    status = crud.add_product(db, data)
+def post_product(data, repo: GenericRepository = Depends(create_repo(models.Product))):
+    status = add_product(data, repo)
     if status == 400:
         raise HTTPException(status_code=400, detail="Wrong data types")
     if status == 409:
@@ -39,18 +31,18 @@ def post_product(data, db: Session = Depends(get_db)):
 
 
 @app.delete("/product/{product_id}", summary="Delete a certain product by id", status_code=204)
-def delete_product(product_id: str, db: Session = Depends(get_db)):
-    db_product = crud.delete_product_by_code(db, product_id=product_id)
+def delete_product(product_id: str, repo: GenericRepository = Depends(create_repo(models.Product))):
+    db_product = repo.delete_by_condition(models.Product.product_id==product_id)
     return db_product
 
 @app.post("/agreement")
-def post_agreement(data, db: Session = Depends(get_db)):
+def post_agreement(data, repo: GenericRepository = Depends(create_repo(models.Product)), repo2: GenericRepository = Depends(create_repo(models.Agreement)),  repo3: GenericRepository = Depends(create_repo(models.Client))):
     errors = ["No product with a given code",
               "Wrong data types",
               "Term is out of range",
               "Interest is out of range",
               "Principle amount is out of range"]
-    agreement = crud.create_agreement(db, data)
+    agreement = create_agreement(repo, repo2, repo3 ,data)
     if agreement <= 0:
         raise HTTPException(status_code=400, detail=errors[-agreement])
     return agreement
